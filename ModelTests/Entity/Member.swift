ec3c68922f9fd6950834a2a0648177c936c8591a
Seyed Samad Gholamzadeh <ssamadgh@gmail.com>
2018-08-26 20:54:46 +0430

    initial commit 2

---
+++ ModelTests/Entity/Member.swift
@@ -0,0 +1,61 @@
+//
+//  member.swift
+//  MyModelLibrary
+//
+//  Created by Seyed Samad Gholamzadeh on 8/23/18.
+//  Copyright © 2018 Seyed Samad Gholamzadeh. All rights reserved.
+//
+
+import Foundation
+
+struct Member: GEntityProtocol, Hashable, Codable {
+	
+	
+	var id: Int
+	var firstName: String
+	var lastName: String
+	var fullName: String {
+		return firstName + " " + lastName
+	}
+	var email: String
+	var gender: String
+	var country: String
+	
+	init?(data: [String : Any]) {
+		self.id = data["id"] as! Int
+		self.firstName = data["first_name"] as? String ?? ""
+		self.lastName = data["last_name"] as? String ?? ""
+		self.email = data["email"] as? String ?? ""
+		self.gender = data["gender"] as? String ?? ""
+		self.country = data["country"] as? String ?? ""
+	}
+	
+	mutating func update(with newFetechedEntity: Member) {
+		let entity = newFetechedEntity
+		self.firstName = entity.firstName
+		self.lastName = entity.lastName
+		self.email = entity.email
+		self.country = entity.country
+	}
+	
+	subscript(key: String) -> String? {
+		
+		if key == "country" {
+			return self.country
+		}
+		
+		return nil
+	}
+	
+	enum CodingKeys: String, CodingKey {
+		case firstName = "first_name"
+		case lastName = "last_name"
+		case id, email, gender, country
+	}
+	
+	
+	/*
+	{"id":1,"first_name":"Orland","last_name":"Stapleford","email":"ostapleford0@bluehost.com","gender":"Male","country":"Indonesia"}
+	*/
+	
+}
--- ModelTests/ModelTests.swift
+++ ModelTests/ModelTests.swift
@@ -10,20 +10,35 @@ import XCTest
 @testable import Model
 
 class ModelTests: XCTestCase {
-    
+	
+	var model: Model<Member>!
+	
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+		self.model = Model()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+		self.model = nil
         super.tearDown()
     }
+	
+	func entities(forFileWithName fileName: String) -> [Member] {
+		let url = Bundle.main.url(forResource: fileName, withExtension: "json")!
+		let json = try! Data(contentsOf: url)
+		
+		let decoder = JSONDecoder()
+		let members = try! decoder.decode([Member].self, from: json)
+		return members
+	}
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+		
+		
     }
     
     func testPerformanceExample() {
